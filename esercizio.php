6423a9feaa9d7c7ee746d4701810ff35b8c63b2c
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-23 14:57:50 +0200

    init project

---
+++ esercizio.php
@@ -0,0 +1,19 @@
+<?php
+  $frase = 'Ciao bella vita, come va? Bella la nuova esperienza di Boolean? Come sta proseguendo il corso? Ciao e buona serata';
+  $censura = $_GET['censura'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    
+</body>
+</html>
